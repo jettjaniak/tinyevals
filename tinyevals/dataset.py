454419668464ea5814ce30a0347e408ce96556a4
menamerai <rphananh@gmail.com>
2024-01-22 11:34:04 -0500

    updated data pull function

--- tinyevals/dataset.py
+++ tinyevals/dataset.py
@@ -1,11 +1,11 @@
 from datasets import load_dataset
 from tqdm.auto import tqdm
 
-def load_txt_dataset(split: str) -> list[str]:
+def load_clean_dataset(split: str, tokenized: bool = False) -> list[str]:
     # checking just startswith, because you can include slice like "train[:1000]"
     assert split.startswith("train") or split.startswith("validation")
-    hf_ds = load_dataset(f"jbrinkma/tinystories-v2-clean", split=split)
+    hf_ds = load_dataset(f"jbrinkma/tinystories-v2-clean{'-tokenized' if tokenized else ''}")
     dataset = []
-    for sample_txt in tqdm(hf_ds["text"]):
+    for sample_txt in tqdm(hf_ds["tokens" if tokenized else "text"]):
         dataset.append(sample_txt)
     return dataset
