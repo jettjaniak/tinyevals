f0ecca910844997e4f1c3643388a45b355ecd76d
Jett <jettjaniak@gmail.com>
2024-01-22 22:06:05 +0100

    load_clean_dataset: split and key fix

--- tinyevals/dataset.py
+++ tinyevals/dataset.py
@@ -4,8 +4,8 @@ from tqdm.auto import tqdm
 def load_clean_dataset(split: str, tokenized: bool = False) -> list[str]:
     # checking just startswith, because you can include slice like "train[:1000]"
     assert split.startswith("train") or split.startswith("validation")
-    hf_ds = load_dataset(f"jbrinkma/tinystories-v2-clean{'-tokenized' if tokenized else ''}")
+    hf_ds = load_dataset(f"jbrinkma/tinystories-v2-clean{'-tokenized' if tokenized else ''}", split=split)
     dataset = []
-    for sample_txt in tqdm(hf_ds["tokens" if tokenized else "text"]):
-        dataset.append(sample_txt)
+    for sample in tqdm(hf_ds["tokens" if tokenized else "story"]):
+        dataset.append(sample)
     return dataset
